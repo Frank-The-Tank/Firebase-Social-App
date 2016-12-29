cbab9fd12907e71cefbe82fa71ba09278e311f8a
FrankGarcia_ <futboler101@yahoo.com>
2016-12-29 15:08:20 -0800

    Updated UIView with custom classes

---
+++ Social App/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social App
+//
+//  Created by Frank Garcia on 12/29/16.
+//  Copyright © 2016 Malibit. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
