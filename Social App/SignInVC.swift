2e5f6862d6c91bd0c3d69c09b672f12264b33244
FrankGarcia_ <futboler101@yahoo.com>
2016-12-29 20:11:34 -0800

    Added Firebase authentication using standard email and the Facebook SDK

--- Social App/SignInVC.swift
+++ Social App/SignInVC.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: EnhancedField!
+    @IBOutlet weak var passwordField: EnhancedField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,59 @@ class SignInVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // Facebook Authentication followed by Firebase Authentication
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("FRANK: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("FRANK: User cancelled Facebook authentication")
+            } else {
+                print("FRANK: Succesfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("FRANK: Unable to authenticate with Firebase = \(error)")
+            } else {
+                print("FRANK: Succesfully authenticated with Firebase")
+            }
+            
+        })
+    }
+    
+    // Standard email verification with Firebase
 
+    @IBAction func signinTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("FRANK: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("FRANK: Unable to authenticate with Firebase")
+                        } else {
+                            print("FRANK: New email user authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
 }
 
