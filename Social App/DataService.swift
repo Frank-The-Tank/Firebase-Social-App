ae3dd063bea70351e50123fab068a0a886542237
Frank-The-Tank <futboler101@yahoo.com>
2017-06-23 16:09:37 -0700

    Added caching to the downloaded images for optimization purposes

--- Social App/DataService.swift
+++ Social App/DataService.swift
@@ -10,15 +10,20 @@ import Foundation
 import Firebase
 
 let DB_BASE = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage().reference()
 
 class DataService {
     
     // Makes the class a singleton (one global class that everything references)
     static let ds = DataService()
-    
+
+    // DB References
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
+    
+    // Storage References
+    private var _REF_POST_IMAGES = STORAGE_BASE.child("post-pics")
     
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
@@ -32,9 +37,14 @@ class DataService {
         return _REF_USERS
     }
     
+    var REF_POST_IMAGES: FIRStorageReference {
+        return _REF_POST_IMAGES
+    }
+    
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         REF_USERS.child(uid).updateChildValues(userData)
     }
+    
     
     
 }
